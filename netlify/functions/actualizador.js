5fc1912a754b7f3ba8390117e9326194c90448ba
meeeww <thezasm03@gmail.com>
2023-06-11 13:19:32 +0200

    Fix with axios

--- netlify/functions/actualizador.js
+++ netlify/functions/actualizador.js
@@ -1,17 +1,35 @@
 import { schedule } from "@netlify/functions";
 import { Webhook } from 'discord-webhook-node';
-import XMLHttpRequest from 'xhr2'
+import axios from 'axios'
 const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
 console.log("init")
 export const handler = schedule("*/1 * * * *", async (event) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        'content': "Probando",
-        'username': 'AI',
-    }));
+    let embeds = [
+        {
+            title: "Webhook Example",
+        },
+    ];
+
+    let data = JSON.stringify({ embeds });
+
+    var config = {
+        method: "POST",
+        url: "https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW", // https://discord.com/webhook/url/here
+        headers: { "Content-Type": "application/json" },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            console.log("Webhook delivered successfully");
+            return response;
+        })
+        .catch((error) => {
+            console.log(error);
+            return error;
+        });
+
     console.log("hola")
     return {
         statusCode: 200,
