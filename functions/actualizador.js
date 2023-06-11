9bfc4db422d454aa8bfcb8a8cb3c6fc6c56791c5
meeeww <thezasm03@gmail.com>
2023-06-11 03:46:46 +0200

    Trying

--- functions/actualizador.js
+++ functions/actualizador.js
@@ -1,7 +1,9 @@
 const { schedule } = require("@netlify/functions");
+import { Webhook } from 'discord-webhook-node';
+const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
-const handler = async function(event, context) {
-    console.log("Received event:", event);
+const handler = async function() {
+    hook.send("test")
 
     return {
         statusCode: 200,
