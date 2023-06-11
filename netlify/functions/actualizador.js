fb7325be079c8af74f145faaeed3657969b91e19
meeeww <thezasm03@gmail.com>
2023-06-11 13:15:19 +0200

    Fix HTTP Request Webhook 2

--- netlify/functions/actualizador.js
+++ netlify/functions/actualizador.js
@@ -1,5 +1,6 @@
 import { schedule } from "@netlify/functions";
 import { Webhook } from 'discord-webhook-node';
+import XMLHttpRequest from 'xhr2'
 const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
 console.log("init")
