d281cd1a925deb0d3a97d716efaae097e1e8da22
meeeww <thezasm03@gmail.com>
2023-06-11 14:07:35 +0200

    Funcion asincrona

--- netlify/functions/actualizador.js
+++ netlify/functions/actualizador.js
@@ -1,38 +1,16 @@
 import { schedule } from "@netlify/functions";
+
 import { Webhook } from 'discord-webhook-node';
-import axios from 'axios'
 const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
 console.log("init")
-export const handler = schedule("*/1 * * * *", async (event) => {
-    let embeds = [
-        {
-            title: "Webhook Example",
-        },
-    ];
-
-    let data = JSON.stringify({ embeds });
-
-    var config = {
-        method: "POST",
-        url: "https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW", // https://discord.com/webhook/url/here
-        headers: { "Content-Type": "application/json" },
-        data: data,
-    };
-
-    axios(config)
-        .then((response) => {
-            console.log("Webhook delivered successfully");
-            return response;
-        })
-        .catch((error) => {
-            console.log(error);
-            return error;
-        });
-
+const handler = async function(event, context) {
+    hook.send("Prueba de la conexión. <@286402429258301440>")
     console.log("hola")
     return {
         statusCode: 200,
         body: "Hello"
     }
-})
+}
+
+exports.handler = schedule("*/1 * * * *", handler)
