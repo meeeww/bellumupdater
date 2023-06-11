f2602e4991a69e98c93a95dca89f04ef8e5c1f09
meeeww <thezasm03@gmail.com>
2023-06-11 13:10:53 +0200

    Webhook fix 1

--- netlify/functions/actualizador.js
+++ netlify/functions/actualizador.js
@@ -4,7 +4,13 @@ const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/k
 
 console.log("init")
 export const handler = schedule("*/1 * * * *", async (event) => {
-    hook.send("Test cada hora (sorry for el flood)")
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        'content': "Probando",
+        'username': 'AI',
+    }));
     console.log("hola")
     return {
         statusCode: 200,
