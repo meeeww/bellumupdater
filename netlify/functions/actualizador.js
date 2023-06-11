47f9c3ee5c2bc8188da761529540c7397a3fc336
meeeww <thezasm03@gmail.com>
2023-06-11 18:42:55 +0200

    Movido a javascript

--- netlify/functions/actualizador.js
+++ netlify/functions/actualizador.js
@@ -28,21 +28,17 @@
 
 // export { handler };
 
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-import { schedule } from "@netlify/functions";
-
 import { Webhook } from 'discord-webhook-node';
 const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
-const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  console.log("1 minuto");
-  await hook.send("Prueba de la conexión. <@286402429258301440>")
+import { schedule } from "@netlify/functions";
 
-  return {
-    statusCode: 200,
-  };
+const handler = async function(event, context) {
+  console.log("1 minuto");
+    await hook.send("Prueba de la conexión. <@286402429258301440>")
+    return {
+        statusCode: 200,
+    };
 };
 
-const handler = schedule("*/1 * * * *", myHandler)
-
-export { handler };
+exports.handler = schedule("*/1 * * * *", handler);
