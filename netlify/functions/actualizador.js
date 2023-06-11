2b9aa36899d5941613db009ddd9619fcbc4df58e
meeeww <thezasm03@gmail.com>
2023-06-11 03:53:29 +0200

    Set de actualizador rango scheduled

--- netlify/functions/actualizador.js
+++ netlify/functions/actualizador.js
@@ -3,7 +3,7 @@ import { Webhook } from 'discord-webhook-node';
 const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
 const handler = async function() {
-    hook.send("test")
+    hook.send("Test cada hora (sorry for el flood)")
 
     return {
         statusCode: 200,
