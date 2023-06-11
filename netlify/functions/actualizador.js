a03db6957db3a2f28fbc6a131b0ec6aaf0e11b3a
meeeww <thezasm03@gmail.com>
2023-06-11 13:04:37 +0200

    Fix 2

--- netlify/functions/actualizador.js
+++ netlify/functions/actualizador.js
@@ -2,13 +2,12 @@ import { schedule } from "@netlify/functions";
 import { Webhook } from 'discord-webhook-node';
 const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
-const handler = async function() {
+console.log("init")
+export const handler = schedule("*/1 * * * *", async (event) => {
     hook.send("Test cada hora (sorry for el flood)")
-    console.log("hey")
-
+    console.log("hola")
     return {
         statusCode: 200,
-    };
-};
-
-exports.handler = schedule("@hourly", handler);
+        body: "Hello"
+    }
+})
