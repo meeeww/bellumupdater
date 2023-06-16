f10ed6d946320f338b6a509485ea96c8f3822fe9
meeeww <thezasm03@gmail.com>
2023-06-16 12:08:29 +0200

    Actualizacion de API

--- netlify/functions/actualizarRangos.js
+++ netlify/functions/actualizarRangos.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import { Webhook } from 'discord-webhook-node';
 const hook = new Webhook("https://discord.com/api/webhooks/1117244669148024953/kYof2ZGML7JOG3qqw0z-1sLJOeN3sgElCOZ1wWvBSIdyT11dTqF-xB88w1n6ykhu14DW");
 
-const API = "RGAPI-206b6873-5ec5-4360-a116-b1f1f2fd0044"
+const API = "RGAPI-9c89909e-770b-4acf-aaa6-9eb07f095247"
 
 
 const handler = async function (event, context) {
